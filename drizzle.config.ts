3b592c965a7f0d22b132c148f28c4e737cd54aad
Lwazi Shabalala <lwazigugushabalala@gmail.com>
2025-01-25 12:10:06 +0200

    Clean up ignored files

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./app/db/schema.ts",  // Path to your schema
-  dbCredentials: {
-    url: "postgresql://postgres.xwrouybxxihamjyzknzf:lwazi12%3F@aws-0-sa-east-1.pooler.supabase.com:6543/postgres",
-  },
-});
--- types/wav-encoder.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'wav-encoder' {
-    const WavEncoder: {
-        encode: (options: { sampleRate: number; channelData: Float32Array[] }) => Promise<ArrayBuffer>;
-    };
-    export default WavEncoder;
-}
