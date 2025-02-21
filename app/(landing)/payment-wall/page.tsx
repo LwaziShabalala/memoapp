986eb7134299a4e3acfa84dce78dd2d7369e383d
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-02-21 23:56:33 +0200

    Update page.tsx currency

--- app/(landing)/payment-wall/page.tsx
+++ app/(landing)/payment-wall/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-
 import React, { useState } from "react";
 import PricingCard from "../../../components/ui/pricingcard";
 
 const PaymentWall = () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
 
     const handleSuccess = (reference: string) => {
@@ -23,13 +21,12 @@ const PaymentWall = () => {
             <p className="text-xl text-gray-600">
                 Please choose one of the following payment options to proceed.
             </p>
-
             <div className="pt-8 grid grid-cols-1 sm:grid-cols-2 gap-8 max-w-4xl mx-auto">
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="1-year access"
-                        originalPrice="R500"
-                        price="R250"
+                        originalPriceUSD={26.01} // R500 / 19.22 (ZAR rate)
+                        priceUSD={13.01} // R250 / 19.22
                         storage="Join now and get early access to exclusive updates and features."
                         users="Be among the first to experience advanced transcription tools and AI-powered features!"
                         sendUp={true}
@@ -37,12 +34,11 @@ const PaymentWall = () => {
                         onCancel={handleCancel}
                     />
                 </div>
-
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="Lifetime Access"
-                        originalPrice="R1000"
-                        price="R500"
+                        originalPriceUSD={52.03} // R1000 / 19.22
+                        priceUSD={26.01} // R500 / 19.22
                         storage="Secure lifetime access with exclusive perks and continuous updates."
                         users="Enjoy permanent access to new features, including priority support and more!"
                         sendUp={true}
@@ -51,7 +47,6 @@ const PaymentWall = () => {
                     />
                 </div>
             </div>
-
             <p className="text-sm text-gray-400 mt-8">
                 By selecting a plan, you agree to our terms of service and privacy policy.
             </p>
