6a84d987a084e3f642c367d1c8d550f89d45cffb
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-02-22 14:12:55 +0200

    Update page.tsx normal

--- app/(landing)/payment-wall/page.tsx
+++ app/(landing)/payment-wall/page.tsx
@@ -1,18 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import PricingCard from "../../../components/ui/pricingcard";
-
 const PaymentWall = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
     const handleSuccess = (reference: string) => {
         console.log("Payment successful, reference:", reference);
         // Handle post-payment logic
     };
-
     const handleCancel = () => {
         console.log("Payment was canceled");
         // Handle cancellation
     };
-
     return (
         <div className="text-center py-20 space-y-8">
             <h1 className="text-4xl font-bold text-gray-800">Choose Your Payment Option</h1>
@@ -23,8 +22,8 @@ const PaymentWall = () => {
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="1-year access"
-                        originalPriceUSD={26.01} // R500 / 19.22 (ZAR rate)
-                        priceUSD={13.01} // R250 / 19.22
+                        originalPrice="R500"
+                        price="R250"
                         storage="Join now and get early access to exclusive updates and features."
                         users="Be among the first to experience advanced transcription tools and AI-powered features!"
                         sendUp={true}
@@ -35,8 +34,8 @@ const PaymentWall = () => {
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="Lifetime Access"
-                        originalPriceUSD={52.03} // R1000 / 19.22
-                        priceUSD={26.01} // R500 / 19.22
+                        originalPrice="R1000"
+                        price="R500"
                         storage="Secure lifetime access with exclusive perks and continuous updates."
                         users="Enjoy permanent access to new features, including priority support and more!"
                         sendUp={true}
@@ -51,5 +50,4 @@ const PaymentWall = () => {
         </div>
     );
 };
-
 export default PaymentWall;
