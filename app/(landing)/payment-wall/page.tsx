2b7cb0c8afb3b50e813179ecad7b90ad9d21739d
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-02-24 16:42:14 +0200

    Update page.tsx ZAR2X

--- app/(landing)/payment-wall/page.tsx
+++ app/(landing)/payment-wall/page.tsx
@@ -75,8 +75,8 @@ const PaymentWall = () => {
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="1-year access"
-                        originalPrice="$500"
-                        price="$250"
+                        originalPrice="R500"
+                        price="R250"
                         storage="Join now and get early access to exclusive updates and features."
                         users="Be among the first to experience advanced transcription tools and AI-powered features!"
                         sendUp={true}
@@ -88,8 +88,8 @@ const PaymentWall = () => {
                 <div className="flex flex-col h-full">
                     <PricingCard
                         title="Lifetime Access"
-                        originalPrice="$1000"
-                        price="$500"
+                        originalPrice="R1000"
+                        price="R500"
                         storage="Secure lifetime access with exclusive perks and continuous updates."
                         users="Enjoy permanent access to new features, including priority support and more!"
                         sendUp={true}
