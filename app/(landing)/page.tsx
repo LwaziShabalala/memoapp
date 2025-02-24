81ff43cfa864000825b84ff0601e73db8a6e3d6d
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-02-24 15:31:49 +0200

    Update page.tsx prices

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -40,8 +40,8 @@ const LandingPage = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                         <PricingCard
                             title="1-year access"
-                            originalPrice="$28"
-                            price="$14"
+                            originalPrice="ZAR1000"
+                            price="ZAR500"
                             storage="Join now and get early access to exclusive updates and features."
                             users="Be among the first to experience advanced transcription tools and AI-powered features!"
                             sendUp={true}
@@ -49,8 +49,8 @@ const LandingPage = () => {
                         />
                         <PricingCard
                             title="Lifetime Access"
-                            originalPrice="$55"
-                            price="$28"
+                            originalPrice="ZAR500"
+                            price="ZAR250"
                             storage="Secure lifetime access with exclusive perks and continuous updates."
                             users="Enjoy permanent access to new features, including priority support and more!"
                             sendUp={true}
