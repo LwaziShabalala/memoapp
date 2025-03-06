697840ef7626765389924f558f6063a71b43b087
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-03-06 09:05:38 +0200

    Update layout.tsx datafast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs"
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -30,6 +32,12 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Script
+            defer
+            data-website-id="67c9482c5891e85373d3e90b"
+            data-domain="memoapp-tawny.vercel.app"
+            src="https://datafa.st/js/script.js"
+          />
           {children}
         </body>
       </html>
