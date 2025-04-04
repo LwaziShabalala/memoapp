b31407777c960b47dc619616756117f41b5597b1
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-04 14:53:04 +0200

    Update page.tsx url

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -39,7 +39,7 @@ const LandingPage = () => {
               storage="Join now and get early access to exclusive updates and features."
               users="Be among the first to experience advanced transcription tools and AI-powered features!"
               sendUp={true}
-              gumroadUrl="https://your-gumroad-url/yearly-plan" // Replace with your actual Gumroad URL
+              gumroadUrl="lwazigugushabalala.gumroad.com/l/" // Replace with your actual Gumroad URL
             />
             <PricingCard
               title="Lifetime Access"
