160bbe0250369b3b64cc3077ce453f2dc550f67c
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-05 05:43:50 +0200

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -40,7 +40,7 @@ const LandingPage = () => {
               storage="Join now and get early access to exclusive updates and features."
               users="Be among the first to experience advanced transcription tools and AI-powered features!"
               sendUp={true}
-              gumroadProductId="YOUR_1YEAR_PRODUCT_ID" // Replace with your actual Gumroad product ID
+              gumroadProductId="https://memoapp.gumroad.com/l/qlale" // Replace with your actual Gumroad product ID
             />
             <PricingCard
               title="Lifetime Access"
