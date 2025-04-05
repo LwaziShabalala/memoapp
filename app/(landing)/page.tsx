f2abd4ae43686bf69c1511f16aa0c6c68b133273
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-05 15:46:42 +0200

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,10 +6,8 @@ import PricingCard from "@/components/ui/pricingcard";
 import VideoComponent from "@/components/landing-video";
 import FeaturesSection from "@/components/landing-features";
 import VirtualizedWrapper from "@/components/virtualized-wrapper";
-
 const LandingPage = () => {
   useUser();
-  
   return (
     <VirtualizedWrapper>
       <div className="min-h-screen bg-gray-950">
@@ -17,7 +15,17 @@ const LandingPage = () => {
         <section className="py-16">
           <LandingHero />
         </section>
-        {/* Pricing Section */}
+        {/* Video Section /}
+        <section className="-mt-8 w-full py-12">
+          <h2 className="text-2xl font-bold text-gray-200 mb-8 text-center">
+            See how it works
+          </h2>
+          <div className="max-w-4xl mx-auto px-8">
+            <VideoComponent />
+          </div>
+        </section>
+        <FeaturesSection />
+        {/ Pricing Section */}
         <section className="max-w-4xl mx-auto px-4 py-20">
           <h2 className="text-3xl font-bold text-gray-200 mb-6 text-center">
             Choose Your Plan
@@ -30,7 +38,6 @@ const LandingPage = () => {
               storage="Join now and get early access to exclusive updates and features."
               users="Be among the first to experience advanced transcription tools and AI-powered features!"
               sendUp={true}
-              checkoutUrl="https://memoapp.lemonsqueezy.com/buy/816dd08c-0bca-4533-9d49-4f9a80793135?embed=1"
             />
             <PricingCard
               title="Lifetime Access"
@@ -39,7 +46,6 @@ const LandingPage = () => {
               storage="Secure lifetime access with exclusive perks and continuous updates."
               users="Enjoy permanent access to new features, including priority support and more!"
               sendUp={true}
-              checkoutUrl="YOUR_LIFETIME_CHECKOUT_URL" // Replace with your lifetime subscription URL
             />
           </div>
         </section>
