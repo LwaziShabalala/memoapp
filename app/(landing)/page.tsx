d1575a142a9f6956cd8642ea09cd4ae77bfb1615
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-05 15:49:41 +0200

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,8 +6,22 @@ import PricingCard from "@/components/ui/pricingcard";
 import VideoComponent from "@/components/landing-video";
 import FeaturesSection from "@/components/landing-features";
 import VirtualizedWrapper from "@/components/virtualized-wrapper";
+import { useRouter } from "next/navigation";
+
 const LandingPage = () => {
-  useUser();
+  const { user } = useUser();
+  const router = useRouter();
+  
+  const handleSuccess = (paymentId: string) => {
+    console.log("Payment successful, reference:", paymentId);
+    router.push("/sign-up");
+  };
+  
+  const handleCancel = () => {
+    console.log("Payment was canceled");
+    // Handle cancellation logic if needed
+  };
+  
   return (
     <VirtualizedWrapper>
       <div className="min-h-screen bg-gray-950">
@@ -15,7 +29,7 @@ const LandingPage = () => {
         <section className="py-16">
           <LandingHero />
         </section>
-        {/* Video Section /}
+        {/* Video Section 
         <section className="-mt-8 w-full py-12">
           <h2 className="text-2xl font-bold text-gray-200 mb-8 text-center">
             See how it works
@@ -25,7 +39,8 @@ const LandingPage = () => {
           </div>
         </section>
         <FeaturesSection />
-        {/ Pricing Section */}
+        */}
+        {/* Pricing Section */}
         <section className="max-w-4xl mx-auto px-4 py-20">
           <h2 className="text-3xl font-bold text-gray-200 mb-6 text-center">
             Choose Your Plan
@@ -38,6 +53,8 @@ const LandingPage = () => {
               storage="Join now and get early access to exclusive updates and features."
               users="Be among the first to experience advanced transcription tools and AI-powered features!"
               sendUp={true}
+              onSuccess={handleSuccess}
+              onCancel={handleCancel}
             />
             <PricingCard
               title="Lifetime Access"
@@ -46,6 +63,8 @@ const LandingPage = () => {
               storage="Secure lifetime access with exclusive perks and continuous updates."
               users="Enjoy permanent access to new features, including priority support and more!"
               sendUp={true}
+              onSuccess={handleSuccess}
+              onCancel={handleCancel}
             />
           </div>
         </section>
@@ -53,4 +72,5 @@ const LandingPage = () => {
     </VirtualizedWrapper>
   );
 };
+
 export default LandingPage;
