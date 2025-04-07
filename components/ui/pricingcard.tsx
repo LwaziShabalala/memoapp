ce625ac141d6cf120821ebe4ed575d2220cd2c32
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-07 19:27:54 +0200

    Update pricingcard.tsx

--- components/ui/pricingcard.tsx
+++ components/ui/pricingcard.tsx
@@ -92,7 +92,7 @@ export const PricingCard: React.FC<PricingCardProps> = ({
         if (!window.paypal) {
             const script = document.createElement("script");
             // Add commit=true parameter to force PayPal to use the redirected flow
-            script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=USD&commit=true`;
+            script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_SANDBOX_CLIENT_ID}&currency=USD&commit=true`;
             script.async = true;
             script.onload = () => {
                 if (window.paypal?.Buttons) {
