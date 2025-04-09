cecb403d1c15019d7b92e21d2e2d0efab2355762
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-09 09:09:43 +0200

    Update landing-video.tsx

--- components/landing-video.tsx
+++ components/landing-video.tsx
@@ -1,13 +1,17 @@
 "use client";
+
 import { useState, useEffect, useRef } from "react";
 import Video from "next-video";
 import landingvideo from "@/videos/memoappvideo.mp4";
+
 const VideoComponent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const containerRef = useRef<HTMLDivElement>(null);
+
   const handleVideoLoad = () => {
     setIsLoading(false);
   };
+
   useEffect(() => {
     if (!isLoading && containerRef.current) {
       const videoElements = containerRef.current.querySelectorAll("video");
@@ -18,18 +22,22 @@ const VideoComponent = () => {
       });
     }
   }, [isLoading]);
+
   return (
     <div className="relative group w-full">
       <div className="absolute inset-0 bg-gradient-to-r from-violet-600/30 to-indigo-600/30 rounded-xl blur-3xl opacity-75 group-hover:opacity-100 transition duration-1000 -z-10"></div>
-      {/ Video Container /}
+
+      {/* Video Container */}
       <div className="relative w-full rounded-xl overflow-hidden ring-1 ring-gray-800/50 shadow-lg before:absolute before:inset-0 before:bg-gradient-to-b before:from-indigo-500/40 before:to-transparent before:blur-3xl before:-z-10">
-        {/ Loading Spinner /}
+        
+        {/* Loading Spinner */}
         {isLoading && (
           <div className="absolute inset-0 flex items-center justify-center bg-gray-800 rounded-lg z-10">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
           </div>
         )}
-        {/ Video */}
+
+        {/* Video */}
         <div ref={containerRef} className="aspect-video w-full">
           <Video
             src={landingvideo}
@@ -46,4 +54,5 @@ const VideoComponent = () => {
     </div>
   );
 };
+
 export default VideoComponent;
