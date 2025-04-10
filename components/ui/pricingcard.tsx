c024444c5a3206f2b691968e1079085191b209b7
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-10 18:42:59 +0200

    Update pricingcard.tsx

--- components/ui/pricingcard.tsx
+++ components/ui/pricingcard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import "@/types/lemonsqueezy";
+import "../../types/lemonsqueezy"; // adjust this based on where pricingcard.tsx lives
+
 
 // Props interface
 interface PricingCardProps {
