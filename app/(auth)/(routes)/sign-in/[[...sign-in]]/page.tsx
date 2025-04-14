87b78a00407b0d6a64c53de644e015005b041df5
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-14 05:36:45 +0200

    Update page.tsx

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,18 +1,4 @@
 import { SignIn } from "@clerk/nextjs"
-
 export default function Page() {
-    return (
-        <SignIn
-            appearance={{
-                elements: {
-                    // Target multiple possible elements that could contain the sign-up link
-                    footerAction: { display: "none" },
-                    footerActionLink: { display: "none" },
-                    footer: { 
-                        "& a[href*='sign-up']": { display: "none" }
-                    }
-                }
-            }}
-        />
-    )
+    return <SignIn />
 }
