16c19e2cac4a62786ad851b705ff4eb4c243b9b5
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-14 12:28:35 +0200

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -69,7 +69,7 @@ const LandingPage = () => {
   { text: "Exclusive access to premium UI features", included: true },
               ]}
               updatedText="Updated 2 months ago"
-              checkoutUrl="https://lwazistore.lemonsqueezy.com/buy/ccfe9bd3-63b3-4c5e-afe8-c53860441e7b"
+              checkoutUrl="https://lwazistore.lemonsqueezy.com/buy/fbf50ea6-eed4-4a16-92d4-5bd3bae29162"
               highlighted={true}
             />
           </div>
