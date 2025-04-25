254fb8c4c8ba31625626738cf95f5ffd3f700098
Lwazi <168469070+LwaziShabalala@users.noreply.github.com>
2025-04-25 09:17:10 +0200

    Update page.tsx

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
-import { SignUp } from "@clerk/nextjs"
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignUp />
+  return (
+    <SignUp 
+      redirectUrl="/dashboard" 
+      routing="path"
+    />
+  );
 }
